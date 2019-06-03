dfb841f800291bc0da536010826fe083206617b6
hirose_shigemoto <hirose_shigemoto@tenda.co.jp>
2019-06-03 10:23:11 +0900

    create project with Spring Initializr

---
+++ src/test/java/com/example/validationtest/validationtest/ValidationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validationtest.validationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
